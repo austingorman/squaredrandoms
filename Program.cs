59154eecb672876642d189960d238b8ea78ee24c
Austin Gorman <austin.gorman@gmail.com>
2018-08-28 21:57:34 -0500

    finished project

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace random
+{
+    class Program
+    {
+        g
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> numbers = new List<int>();
+            for (int i = 0; i <= 20; i++)
+            {
+                numbers.Add(random.Next(1, 51));
+
+            };
+
+            List<Double> squareNumList = new List<Double>();
+
+            foreach (var nums in numbers)
+            {
+                // double squared = Math.Pow(nums, 2);
+                // List<int> squared = new List<int>();
+                squareNumList.Add(Math.Pow(nums, 2));
+            }
+            squareNumList.RemoveAll(i => i % 2 != 0);
+            foreach (var oddNums in squareNumList)
+            {
+                Console.WriteLine(oddNums);
+            }
+        }
+    }
+}
